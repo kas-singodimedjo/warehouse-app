fa13af47fddfb39e3cf731f8174707622e1f09a9
kas-singodimedjo <kas.singodimedjo@outlook.com>
2025-06-25 11:45:22 +0200

    login pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Navigate, Route, Routes} from "react-router-dom";
+import Home from './pages/home/Home.jsx';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+          <Route path="/" element={<Home/>}></Route>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import './Home.css'
+import logo from "/src/assets/logo.svg"
+import handleSubmit from "/src/helpers/handleSubmit.js"
+
+function Home() {
+    return (
+        <>
+        <h1>Warehouse</h1>
+        <img src={logo} alt="logo" />
+            <form onSubmit={handleSubmit}>
+                <label><input type="text" name="username" placeholder="gebruikersnaam"/></label>
+                <label><input type="password" name="password" placeholder="wachtwoord"/></label>
+                <button type="submit">login</button>
+            </form>
+        </>
+    )
+}
+
+export default Home;
